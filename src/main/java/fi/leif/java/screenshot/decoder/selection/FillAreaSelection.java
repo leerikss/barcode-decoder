0f94e2d532255d8c1660f0bf355a127103be705a
Leif Eriksson <leerikss@gmail.com>
2014-10-21 22:44:49 +0300

    Fixes windows bug (maybe)

--- src/main/java/fi/leif/java/screenshot/decoder/selection/FillAreaSelection.java
+++ src/main/java/fi/leif/java/screenshot/decoder/selection/FillAreaSelection.java
@@ -55,11 +55,7 @@ public class FillAreaSelection {
                 JFrame frame = new JFrame("Decoder");
                 frame.setUndecorated(true);
                 frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setLayout(new BorderLayout());
                 frame.add(new BackgroundPane());
-                frame.pack();
-                frame.setLocationRelativeTo(null);
                 frame.setVisible(true);
                 frame.setCursor(Cursor.CROSSHAIR_CURSOR);
             }
